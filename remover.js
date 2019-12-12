21fcdbbbedee48d5184837721d1f227133fc4bc6
flakboy <kacper1609@wp.pl>
2019-12-12 18:43:03 +0100

    Pierwszy commit

---
+++ remover.js
@@ -0,0 +1,43 @@
+censoredSrc = ["https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png",
+	"https://static.xx.fbcdn.net/images/emoji.php/v9/t29/1.5/16/1f602.png",
+	"https://static.xx.fbcdn.net/images/emoji.php/v9/ta/1.5/28/1f602.png",
+	"https://static.xx.fbcdn.net/images/emoji.php/v9/t31/1.5/128/1f602.png"]
+
+censoredBg = ['url("https://static.xx.fbcdn.net/images/emoji.php/v9/t29/1.5/16/1f602.png")', 'url("https://www.facebook.com/images/emoji.php/v9/tdc/1.5/16/1f60e.png")',
+	'url("https://www.facebook.com/images/emoji.php/v9/t29/1.5/16/1f602.png")', 'url("https://www.facebook.com/images/emoji.php/v9/t29/1.5/16/1f602.png")',
+	'url("https://static.xx.fbcdn.net/images/emoji.php/v9/t8a/1/32/1f602.png")', 'url("https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png")']
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserve
+monitor = new MutationObserver(function (mutations, observer) {
+
+
+	spans = ["_6qdm", "_3gl1", "_21wj", "_1ift", "_1ifu", "_5zft", "_19_s"]
+	emoji = []
+
+	for (spanClass of spans) {
+		toAdd = Array.prototype.slice.call(document.getElementsByClassName(spanClass));
+		emoji = emoji.concat(toAdd);
+	}
+
+	if (emoji.length > 0) {
+		for (i = 0; i < emoji.length; i++) {
+			if (emoji[i].tagName == "IMG") {
+				if (censoredSrc.includes(emoji[i].src)) {
+					emoji[i].src = "https://cdn.worldvectorlogo.com/logos/adobe-xd.svg";
+				}
+			} else {
+				if (censoredBg.includes(emoji[i].style.backgroundImage)) {
+					emoji[i].style.backgroundImage = 'url("https://cdn.worldvectorlogo.com/logos/adobe-xd.svg")'
+				}
+			}
+		}
+	}
+});
+
+monitor.observe(document, {
+	subtree: true,
+	attributes: true,
+	childList: true
+});
+
+
