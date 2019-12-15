616f7bfb16e161228d2b7f7f88e79874aeb15724
flakboy <kacper1609@wp.pl>
2019-12-15 21:18:19 +0100

    rozbudowanie struktury plików

---
+++ config.js
@@ -0,0 +1,7 @@
+/*
+chrome.storage.local.get(["xd"], function (result) {
+	document.getElementById("value").innerText = result.xd
+})
+*/
+
+document.body.style.color = "red"
--- remover.js
+++ remover.js
@@ -1,17 +1,21 @@
-censoredSrc = ["https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png",
+chrome.storage.local.set({ xd: "xid" })
+
+var censoredSrc = ["https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png",
 	"https://static.xx.fbcdn.net/images/emoji.php/v9/t29/1.5/16/1f602.png",
 	"https://static.xx.fbcdn.net/images/emoji.php/v9/ta/1.5/28/1f602.png",
 	"https://static.xx.fbcdn.net/images/emoji.php/v9/t31/1.5/128/1f602.png"]
 
-censoredBg = ['url("https://static.xx.fbcdn.net/images/emoji.php/v9/t29/1.5/16/1f602.png")', 'url("https://www.facebook.com/images/emoji.php/v9/tdc/1.5/16/1f60e.png")',
+var censoredBg = ['url("https://static.xx.fbcdn.net/images/emoji.php/v9/t29/1.5/16/1f602.png")', 'url("https://www.facebook.com/images/emoji.php/v9/tdc/1.5/16/1f60e.png")',
 	'url("https://www.facebook.com/images/emoji.php/v9/t29/1.5/16/1f602.png")', 'url("https://www.facebook.com/images/emoji.php/v9/t29/1.5/16/1f602.png")',
 	'url("https://static.xx.fbcdn.net/images/emoji.php/v9/t8a/1/32/1f602.png")', 'url("https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png")']
+
+var spans = ["_6qdm", "_3gl1", "_21wj", "_1ift", "_1ifu", "_5zft", "_19_s"]
+
+filterImg = "https://cdn.worldvectorlogo.com/logos/adobe-xd.svg"
 
 MutationObserver = window.MutationObserver || window.WebKitMutationObserve
 monitor = new MutationObserver(function (mutations, observer) {
 
-
-	spans = ["_6qdm", "_3gl1", "_21wj", "_1ift", "_1ifu", "_5zft", "_19_s"]
 	emoji = []
 
 	for (spanClass of spans) {
@@ -23,11 +27,11 @@ monitor = new MutationObserver(function (mutations, observer) {
 		for (i = 0; i < emoji.length; i++) {
 			if (emoji[i].tagName == "IMG") {
 				if (censoredSrc.includes(emoji[i].src)) {
-					emoji[i].src = "https://cdn.worldvectorlogo.com/logos/adobe-xd.svg";
+					emoji[i].src = filterImg;
 				}
 			} else {
 				if (censoredBg.includes(emoji[i].style.backgroundImage)) {
-					emoji[i].style.backgroundImage = 'url("https://cdn.worldvectorlogo.com/logos/adobe-xd.svg")'
+					emoji[i].style.backgroundImage = 'url("' + filterImg + '")'
 				}
 			}
 		}
