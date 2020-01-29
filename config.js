328cf69a58f2a47c73d9753eba3b86bb6c2fcf83
flakboy <kacper1609@wp.pl>
2020-01-29 11:12:42 +0100

    Added dialog.html to configurate the extension

--- config.js
+++ config.js
@@ -1,7 +1,34 @@
+//chrome.storage.local.set({ xd: "xid" })
 /*
 chrome.storage.local.get(["xd"], function (result) {
 	document.getElementById("value").innerText = result.xd
 })
 */
 
-document.body.style.color = "red"
+img_srcs = document.getElementsByClassName("settings__file")
+
+for (let src of img_srcs) {
+	src.onchange = () => {
+		var fields = document.getElementsByClassName("settings__address")
+		for (let field of fields) {
+			field.disabled = true
+		}
+		document.getElementById(src.dataset.field).disabled = false
+	}
+}
+
+form = document.getElementById("settings")
+
+form.onsubmit = () => {
+	var url = document.getElementById("url")
+
+	if (url.value.length) {
+		chrome.storage.local.set({ xd: url.value })
+	}
+
+	chrome.storage.local.get(["xd"], function (result) {
+		//alert(result.xd)
+		alert("Pomyślnie zapisano ustawienia")
+	})
+	return false
+}
--- remover.js
+++ remover.js
@@ -1,22 +1,30 @@
-chrome.storage.local.set({ xd: "xid" })
-
-var censoredSrc = ["https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png",
+const censoredSrc = [
+	"https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png",
 	"https://static.xx.fbcdn.net/images/emoji.php/v9/t29/1.5/16/1f602.png",
 	"https://static.xx.fbcdn.net/images/emoji.php/v9/ta/1.5/28/1f602.png",
-	"https://static.xx.fbcdn.net/images/emoji.php/v9/t31/1.5/128/1f602.png"]
+	"https://static.xx.fbcdn.net/images/emoji.php/v9/t31/1.5/128/1f602.png",
+	"https://static.xx.fbcdn.net/images/emoji.php/v9/t8a/1/32/1f602.png"
+]
 
-var censoredBg = ['url("https://static.xx.fbcdn.net/images/emoji.php/v9/t29/1.5/16/1f602.png")', 'url("https://www.facebook.com/images/emoji.php/v9/tdc/1.5/16/1f60e.png")',
-	'url("https://www.facebook.com/images/emoji.php/v9/t29/1.5/16/1f602.png")', 'url("https://www.facebook.com/images/emoji.php/v9/t29/1.5/16/1f602.png")',
-	'url("https://static.xx.fbcdn.net/images/emoji.php/v9/t8a/1/32/1f602.png")', 'url("https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png")']
+const censoredBg = [
+	'url("https://static.xx.fbcdn.net/images/emoji.php/v9/t29/1.5/16/1f602.png")',
+	'url("https://www.facebook.com/images/emoji.php/v9/tdc/1.5/16/1f60e.png")',
+	'url("https://www.facebook.com/images/emoji.php/v9/t29/1.5/16/1f602.png")',
+	'url("https://www.facebook.com/images/emoji.php/v9/t29/1.5/16/1f602.png")',
+	'url("https://static.xx.fbcdn.net/images/emoji.php/v9/t8a/1/32/1f602.png")',
+	'url("https://static.xx.fbcdn.net/images/emoji.php/v9/te3/1.5/32/1f602.png")'
+]
 
-var spans = ["_6qdm", "_3gl1", "_21wj", "_1ift", "_1ifu", "_5zft", "_19_s"]
+const spans = ["_6qdm", "_3gl1", "_21wj", "_1ift", "_1ifu", "_5zft", "_19_s"]
 
-filterImg = "https://cdn.worldvectorlogo.com/logos/adobe-xd.svg"
+chrome.storage.local.get(["xd"], result => {
+	filterImg = result.xd
+})
 
 MutationObserver = window.MutationObserver || window.WebKitMutationObserve
-monitor = new MutationObserver(function (mutations, observer) {
+monitor = new MutationObserver((mutations, observer) => {
 
-	emoji = []
+	var emoji = []
 
 	for (spanClass of spans) {
 		toAdd = Array.prototype.slice.call(document.getElementsByClassName(spanClass));
@@ -24,15 +32,15 @@ monitor = new MutationObserver(function (mutations, observer) {
 	}
 
 	if (emoji.length > 0) {
-		for (i = 0; i < emoji.length; i++) {
-			if (emoji[i].tagName == "IMG") {
-				if (censoredSrc.includes(emoji[i].src)) {
-					emoji[i].src = filterImg;
+		for (span of emoji) {
+			if (span.tagName == "IMG") {
+				if (censoredSrc.includes(span.src)) {
+					span.src = filterImg;
 				}
-			} else {
-				if (censoredBg.includes(emoji[i].style.backgroundImage)) {
-					emoji[i].style.backgroundImage = 'url("' + filterImg + '")'
-				}
+			} else if (censoredBg.includes(span.style.backgroundImage)) {
+				span.style.backgroundImage = 'url("' + filterImg + '")';
+				span.style.backgroundPosition = "center";
+				span.style.backgroundSize = "cover";
 			}
 		}
 	}
